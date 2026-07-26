5e4cae448c277525c4f6da424b0889d4bedbc647
hangost <u6008737@utah.edu>
2026-07-26 21:26:27 +0900

    Update MiXeR.Bi.sh

--- MiXeR.Bi.sh
+++ MiXeR.Bi.sh
@@ -28,12 +28,12 @@ python ${mixer_p}/mixer/precimed/mixer.py fit2 \
 	--threads 12
 
 python ${mixer_p}/mixer/precimed/mixer.py test2 \
-        --trait1-file ${oper_f}/1.sumstats/${st1}/MiXeR.csv/${st1##*/}.MiXer.qc.noMHCsumstats \
-        --trait2-file ${oper_f}/1.sumstats/${st2}/MiXeR.csv/${st2##*/}.MiXer.qc.noMHCsumstats \
-        --load-params-file ${oper_f}/3.MiXeR.bi/${g}/${st1##*/}_${st2##*/}/rand/${st1##*/}_${st2##*/}.${nm}.fit.json \
-        --out ${oper_f}/3.MiXeR.bi/${g}/${st1##*/}_${st2##*/}/rand/${st1##*/}_${st2##*/}.${nm}.test \
-        --bim-file ${oper_f}/ref/TGP.ref/1000G_EUR_Phase3_plink/1000G.EUR.QC.@.bim \
-        --ld-file ${oper_f}/ref/TGP.ref/test.ld/1000G.EUR.QC.@.run4.ld \
-        --lib ${mixer_p}/mixer/src/build/lib/libbgmg.so \
-        --threads 12
+	--trait1-file ${oper_f}/1.sumstats/${st1}/MiXeR.csv/${st1##*/}.MiXer.qc.noMHCsumstats \
+    --trait2-file ${oper_f}/1.sumstats/${st2}/MiXeR.csv/${st2##*/}.MiXer.qc.noMHCsumstats \
+    --load-params-file ${oper_f}/3.MiXeR.bi/${g}/${st1##*/}_${st2##*/}/rand/${st1##*/}_${st2##*/}.${nm}.fit.json \
+    --out ${oper_f}/3.MiXeR.bi/${g}/${st1##*/}_${st2##*/}/rand/${st1##*/}_${st2##*/}.${nm}.test \
+    --bim-file ${oper_f}/ref/TGP.ref/1000G_EUR_Phase3_plink/1000G.EUR.QC.@.bim \
+    --ld-file ${oper_f}/ref/TGP.ref/test.ld/1000G.EUR.QC.@.run4.ld \
+    --lib ${mixer_p}/mixer/src/build/lib/libbgmg.so \
+    --threads 12
 
